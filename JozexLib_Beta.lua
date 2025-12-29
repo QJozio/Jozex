3d8cab727e9f3f01710032646bb56cf319c0f98b
QJozio <iffyjozia@gmail.com>
2025-12-29 15:15:20 +0800

    Update JozexLib_Beta.lua

--- JozexLib_Beta.lua
+++ JozexLib_Beta.lua
@@ -1,20 +1,40 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+--// Rayfield Loader
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
+--// Services
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+--// Safety check
+if not LocalPlayer:FindFirstChild("CoreStats") then
+    Rayfield:Notify({
+        Title = "Error",
+        Content = "CoreStats not found. Join Bee Swarm first.",
+        Duration = 5
+    })
+    return
+end
+
+--// Window
 local Window = Rayfield:CreateWindow({
-   Name = "Jozex Hub | Bee Swarm",
-   LoadingTitle = "Jozex Hub",
-   LoadingSubtitle = "by QJozio",
-   ConfigurationSaving = { Enabled = false }
+    Name = "Jozex Hub | Bee Swarm",
+    LoadingTitle = "Jozex Hub",
+    LoadingSubtitle = "by QJozio",
+    ConfigurationSaving = {
+        Enabled = false
+    }
 })
 
-local FieldTab = Window:CreateTab("Fields", 4483362458) -- Map Icon
-local StatsTab = Window:CreateTab("Stats", 4483362458) -- Chart Icon
+--// Tabs
+local FieldTab = Window:CreateTab("Fields", 4483362458)
+local StatsTab = Window:CreateTab("Stats", 4483362458)
 
--- Variables
-local SelectedField = ""
-local StartHoney = game.Players.LocalPlayer.CoreStats.Honey.Value
+--// Variables
+local SelectedField = "Sunflower"
+local StartHoney = LocalPlayer.CoreStats.Honey.Value
 local StartTime = tick()
 
+--// Field Positions
 local FieldPositions = {
     ["Sunflower"] = Vector3.new(-208, 4, 185),
     ["Mushroom"] = Vector3.new(-94, 4, 116),
@@ -28,55 +48,72 @@ local FieldPositions = {
     ["Rose"] = Vector3.new(-322, 20, 124)
 }
 
--- [[ FIELDS TAB ]] --
+--// FIELDS TAB
 FieldTab:CreateSection("Teleports")
 
 FieldTab:CreateDropdown({
-   Name = "Select Field",
-   Options = {"Sunflower", "Mushroom", "Dandelion", "Blue Flower", "Clover", "Spider", "Bamboo", "Strawberry", "Pine Tree", "Rose"},
-   CurrentOption = {"Sunflower"},
-   MultipleOptions = false,
-   Callback = function(Option)
-      SelectedField = Option[1]
-   end,
+    Name = "Select Field",
+    Options = {
+        "Sunflower","Mushroom","Dandelion","Blue Flower","Clover",
+        "Spider","Bamboo","Strawberry","Pine Tree","Rose"
+    },
+    CurrentOption = {"Sunflower"},
+    MultipleOptions = false,
+    Callback = function(Option)
+        SelectedField = Option[1]
+    end
 })
 
 FieldTab:CreateButton({
-   Name = "Teleport to Field",
-   Callback = function()
-      local pos = FieldPositions[SelectedField]
-      if pos then
-          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos + Vector3.new(0, 5, 0))
-          -- Reset Stats for new field
-          StartHoney = game.Players.LocalPlayer.CoreStats.Honey.Value
-          StartTime = tick()
-          Rayfield:Notify({Title = "Teleported", Content = "Stats reset for " .. SelectedField, Duration = 3})
-      end
-   end,
+    Name = "Teleport to Field",
+    Callback = function()
+        local char = LocalPlayer.Character
+        if char and char:FindFirstChild("HumanoidRootPart") then
+            local pos = FieldPositions[SelectedField]
+            if pos then
+                char.HumanoidRootPart.CFrame = CFrame.new(pos + Vector3.new(0,5,0))
+                StartHoney = LocalPlayer.CoreStats.Honey.Value
+                StartTime = tick()
+                Rayfield:Notify({
+                    Title = "Teleported",
+                    Content = "Stats reset for "..SelectedField,
+                    Duration = 3
+                })
+            end
+        end
+    end
 })
 
--- [[ STATS TAB ]] --
+--// STATS TAB
 StatsTab:CreateSection("Hourly Rates")
 
 local HPHLabel = StatsTab:CreateLabel("Honey/HR: Calculating...")
 local SessionLabel = StatsTab:CreateLabel("Session: 0")
 
--- Update Loop
+--// Number Formatter
+local function Format(n)
+    if n >= 1e12 then
+        return string.format("%.2fT", n/1e12)
+    elseif n >= 1e9 then
+        return string.format("%.2fB", n/1e9)
+    elseif n >= 1e6 then
+        return string.format("%.2fM", n/1e6)
+    elseif n >= 1e3 then
+        return string.format("%.1fK", n/1e3)
+    else
+        return tostring(math.floor(n))
+    end
+end
+
+--// Update Loop
 task.spawn(function()
     while task.wait(1) do
-        local Elapsed = tick() - StartTime
-        local Gained = game.Players.LocalPlayer.CoreStats.Honey.Value - StartHoney
-        local Rate = (Gained / Elapsed) * 3600
-        
-        local function Format(n)
-            if n >= 1e12 then return string.format("%.2fT", n/1e12)
-            elseif n >= 1e9 then return string.format("%.2fB", n/1e9)
-            elseif n >= 1e6 then return string.format("%.2fM", n/1e6)
-            elseif n >= 1e3 then return string.format("%.1fK", n/1e3)
-            else return tostring(math.floor(n)) end
+        local elapsed = tick() - StartTime
+        if elapsed > 0 then
+            local gained = LocalPlayer.CoreStats.Honey.Value - StartHoney
+            local rate = (gained / elapsed) * 3600
+            HPHLabel:Set("Honey/HR: "..Format(rate))
+            SessionLabel:Set("Session: "..Format(gained))
         end
-
-        HPHLabel:Set("Honey/HR: " .. Format(Rate))
-        SessionLabel:Set("Session: " .. Format(Gained))
     end
 end)
